94ff5e6931c8debc92750988b5d085e57a1a56e9
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2023-12-02 00:44:37 +0100

    day02: init restructure

---
+++ day-02/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn process_part1(input: &str) -> u64 {
+    todo!()
+}
+
+pub fn process_part2(input: &str) -> u64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process_part1() {
+        todo!();
+        let input = "";
+        assert_eq!(0_u64, process_part1(input));
+    }
+
+    #[test]
+    fn test_process_part2() {
+        let input = "";
+        assert_eq!(0_u64, process_part2(input));
+    }
+}
